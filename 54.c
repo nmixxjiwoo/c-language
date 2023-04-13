134de772a923e00ef0f8165448be32364ae119a4
장민성 <bible7568@gmail.com>
2023-04-13 06:23:08 +0000

    aqsw

---
+++ 54.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() 
+{
+    int a, a1, 1a; //변수명은 숫자가 앞에 올수없다.
+	int _a, __a, ___;
+	int if; //예약어는 변수명으로 사용 할 수 없다.
+		
+	return 0;
+	
+}
---
+++ 55.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 3; //변수선언 및 초기화
+	a = a + 6;
+	if(a > 5){ 
+		printf("%d\n", a);
+    }else {
+	    printf("%d\n", a-2);
+    }  
+	return 0;
+}
